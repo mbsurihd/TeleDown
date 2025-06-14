7c65da7361eb5e318a48bf91ae7df296d4089dd0
Thunder66 <thunderprince66@gmail.com>
2025-06-14 06:28:48 +0600

    Fixed README.md

--- install.ps1
+++
@@ -1,5 +0,0 @@
-python.exe -m venv .venv
-.\.venv\Scripts\Activate.ps1
-
-pip.exe install -r requirements.txt
-deactivate
--- run.ps1
+++
@@ -1,6 +0,0 @@
-Set-Location $PSScriptRoot
-
-.venv\Scripts\Activate.ps1
-python.exe main.py
-deactivate
-Read-Host "Press Entery to exit..."
